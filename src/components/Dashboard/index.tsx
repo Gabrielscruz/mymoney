b6c7d0835aaacc1aff9b59b847443a514b52ffe0
Gabrielscruz <54606517+Gabrielscruz@users.noreply.github.com>
2022-02-12 22:56:32 -0300

    Summary

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from "../Summary"
+import { Container } from "./Styles"
+
+export function Dashboard(){
+    return (
+        <Container>
+            <Summary/>
+        </Container>
+    )
+}
